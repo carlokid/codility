903b3f8bbe71bb2cb3b37bcee7107cec2f5a145e
carlokid <charles.adison@gmail.com>
2016-12-11 13:45:35 +0800

    OddOccurencesInArray

---
+++ OddOccurencesInArray.java
@@ -0,0 +1,21 @@
+// you can also use imports, for example:
+// import java.util.*;
+
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] A) {
+        // write your code in Java SE 8
+        Arrays.sort(A);
+        for(int i=0; i<A.length; i++){
+            if(i%2==0){
+                if(i==A.length-1) return A[i];
+                if(A[i]!=A[i+1]){
+                    return A[i];
+                }
+            }
+        }
+        return -1;
+    }
+}
